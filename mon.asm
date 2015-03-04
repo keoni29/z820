c723bf5a0c81ac16e05321a5818baf7bd2065c56
keoni29 <8by8mail@gmail.com>
2015-03-04 23:17:35 +0100

    Bug in hex parser

--- mon.asm
+++ mon.asm
@@ -5,7 +5,10 @@ SHWMSG	.equ	ROM + 6
 
 RAM	.equ 	$2000
 RX	.equ	RAM + 0				; 256 byte input buffer
-STACK	.equ	RAM + 256 + 128			; 128 byte stack
+STACK	.equ	RX + 256 + 127			; 128 byte stack
+VAR	.equ	STACK + 1
+MODE	.equ	VAR + 0
+
 #include "uart.inc"
 
 	.org	RAM + $1000 - 4
@@ -21,8 +24,8 @@ NOTCR		cp	$08			; Backspace?
 		jr	z,BACKSPACE		; Yes, backspace
 		cp	$1B
 		jr	z,ESCAPE
-		inc	bc			; Increment text index
-		jr	NEXTCHAR		; Next char
+		inc	c			; Increment text index
+		jp	p,NEXTCHAR		; Next char
 ESCAPE		ld	a,$5C			; Print '\'
 		call	ECHO
 NEWLINE		ld	a,$0A			; CRNL
@@ -51,14 +54,83 @@ NEXTCHAR	in	a,(U1_LSR)
 		jr	c,LOWERCASE		; Nope, don't convert
 		and	$5F			; Convert to uppercase
 LOWERCASE	push	AF
-		call	ECHO			; Echo character
+			call	ECHO			; Echo character
 		pop	AF
 		ld	(bc),a			; Write to buffer
 		cp	$0D			; Is CR?
 		jr	nz,NOTCR		; No, go back
 		ld	a,$0A
 		call	ECHO
+		ld	c,0
+SETSTOR		sla a				; Set storage mode
+SETMODE		ld	(MODE),a		; Set mode
+BLSKIP		ld	a,'~'
+		call	ECHO
+		inc	c			; Skip character
+NEXTITEM	ld	a,(bc)			; Get character
+		cp	$0D			; CR?
+		jr	z,NEWLINE		; Yes, done this line.
+NEXTHEX		call	OutHex8
+		ld	a,$0D
+		call	ECHO
+		ld	a,$0A
+		call	ECHO
+
+		ld	a,(bc)			; Get character
+		xor	$30			; Map digits to $0-9
+		cp	$0A			; Digit?
+	jr	c,DDIG;DIG			; Yes
+		add	a,$89			; Map letter "A"-"F" to $FA-$FF
+		cp	$FA			; Hex letter?
+		jr	c, NOTHEX		; No, character not hex
+DDIG	inc c
+	jp NEXTHEX
+	;(...)
+DIG		sla	a
+		sla	a			; Hex digit MSD of A.
+		sla	a
+		sla	a
+		ld	b,$4			; Shift count
+HEXSHIFT	sla	a			; Hex digit left MSB to carry.
+		rl	l			; Rotate into LSD
+		rl	h			; Rotate into MSD's
+		djnz	HEXSHIFT		; Repeat 4 times
+		inc	c			; Advance text index
+		jr	NEXTHEX			; Check next
+NOTHEX		ld	a,e
+		cp	c			; Check if L, H empty (no hex digits)
+		jr	nz,NOESCAPE		; Branch out of range, so bit of a hack here.
 		jp	ESCAPE
+RUN		call	ACTRUN
+		jp	SOFTRESET
+ACTRUN		jp	(hl)
+NOESCAPE	ld	a,'@'
+		call	ECHO
+		jp	ESCAPE
+
+;Display a 16- or 8-bit number in hex.
+DispHLhex
+; Input: HL
+		ld  c,h
+		call  OutHex8
+		ld  c,l
+OutHex8
+; Input: C
+		ld  a,c
+		rra
+		rra
+		rra
+		rra
+		call  Conv
+		ld  a,c
+Conv
+		and  $0F
+		add  a,$90
+		daa
+		adc  a,$40
+		daa
+		call ECHO
+		ret
 
 MSG1	.db "Z820 System configuration:",$0D,$0A
 	.db "    Total memory size:     128 KBytes",$0D,$0A
